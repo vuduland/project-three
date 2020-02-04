90ec255696ae5ab38fbc3e95aa964bca7546f047
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-04 00:13:59 -0600

    I think this might be the best option to start with; need to add the .env file and hide the username and password inside the mongoDB URI; I've forgotten how to, so tomorrow, but once we hide that it seems straight forward to connect app to mLab

---
+++ client/build/precache-manifest.236238ea24c2ad2c29922e47b00366c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc072342d629c2a09dd",
+    "url": "/static/css/main.cf747f0c.chunk.css"
+  },
+  {
+    "revision": "9fc072342d629c2a09dd",
+    "url": "/static/js/main.ee9104ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "9f0f695679ecbb7015e6b6f8bec9c568",
+    "url": "/index.html"
+  }
+];
