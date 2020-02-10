af5b545534319fc0dfc8706738e5ed55a94ea1e9
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-10 17:58:03 -0600

    mongo atlas connection

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  const collection = client
+    .db('sample_airbnb')
+    .collection('listingsAndReviews');
+  console.log(collection + 'hi');
+  // perform actions on the collection object
+  client.close();
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
