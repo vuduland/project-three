8e0a1c19ba74ab03323ade67d382ef8d1a9c66cc
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-10 20:25:13 -0600

    testing api stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
-import GoogleApiWrapper from "./components/Map/Map"
+import React, { Component } from 'react';
+import GoogleApiWrapper from './components/Map/Map';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className='App'>
+        <div className='App-header'>
           <h2>KC Cleanup Project</h2>
-          <GoogleApiWrapper/>
+          <GoogleApiWrapper />
         </div>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 require('dotenv').config()
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  const collection = client.db('heroku_ztzn9n3k').collection('test');
+
+  console.log(
+    'referencing ' + collection.foo + ' in the database. connected. hi.'
+  );
+  // perform actions on the collection object
+  client.close();
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
