57556642b6a7045ca3f87fa4a9556e25cee27097
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-11 20:12:41 -0600

    models and routes

--- models/trashtagModel.js
+++ models/trashtagModel.js
@@ -1,29 +1,27 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-var Schema = mongoose.Schema;
-
-var TagSchema = new Schema({
-
-    address: {
-        type: String,
-        trim: true
-    },
-    lat: {
-        type: String,
-        trim: true,
-        required: "Latitutde is Required"
-    },
-    long: {
-        type: String,
-        trim: true,
-        required: "Longitude is Required"
-    },
-    date: {
-        type: Date,
-        trim: true
-    }
+const TagSchema = new Schema({
+  address: {
+    type: String,
+    trim: true
+  },
+  lat: {
+    type: String,
+    trim: true,
+    required: [true, 'Latitutde is Required']
+  },
+  long: {
+    type: String,
+    trim: true,
+    required: [true, 'Longitude is Required']
+  },
+  date: {
+    type: Date,
+    trim: true
+  }
 });
 
-var Tag = mongoose.model("TrashTag", TagSchema);
+const Tag = mongoose.model('TrashTag', TagSchema);
 
 module.exports = Tag;
