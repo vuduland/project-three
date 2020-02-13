164d9c1fc85c722cde3ae7bd2d65b55882a7576e
J. Smith <joe.a.smith@gmail.com>
2020-02-12 20:51:51 -0600

    Working Map Location

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import GoogleApiWrapper from './components/Map/Map';
+import GoogleApiWrapper from './components/DisplayMap/DisplayMap';
+import Nav from './components/Nav/Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <h2>KC Cleanup Project</h2>
-          <GoogleApiWrapper />
-        </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <>
+        <Nav />
+        <GoogleApiWrapper />
+      </>
     );
   }
 }
