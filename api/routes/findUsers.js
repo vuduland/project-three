6d8a7c1fccbcd38edfb4896f78475324278af88d
Peter Smith <petergbsmith@gmail.com>
2020-02-19 20:31:34 -0600

    added login and register routes

---
+++ api/routes/findUsers.js
@@ -0,0 +1,24 @@
+import passport from 'passport';
+
+module.exports = app => {
+    app.get('/findUser', (req, res, next) => {
+        passport.authenticate('jwt', { session: false }, (err, user, info) => {
+            if (err) {
+                console.log(err);
+            }
+            if (info != undefined) {
+                console.log(info.message);
+                res.send(info.message);
+            } else {
+                console.log('user found in db from route');
+                res.status(200).send({
+                    auth: true,
+                    username: user.username,
+                    passworkd: user.password,
+                    email: user.email,
+                    message: 'user found in db',
+                });
+            }
+        })(req, res, next);
+    });
+};
