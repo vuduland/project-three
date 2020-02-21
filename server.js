23e2fb21c27e5e9788d0461804e0a405b7785fc9
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-20 18:46:38 -0600

    where we left off 02.20

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require('express');
-
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the KC-Cleanup API...' })
-);
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  if (err) {
+    // console.error();
+    console.log(err.message);
+  } else {
+    const collection = client.db('kc-cleanup').collection('Users');
+    console.log(`referencing ${collection} in the db. connected. hi.`);
+  }
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  if (res) {
+    console.log('CONNECTED TO NETWORK MAYBE');
+  }
+  // res.json({ msg: 'Welcome to the KC-Cleanup API...' });
+});
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/pins'));
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
