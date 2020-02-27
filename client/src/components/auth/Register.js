d373aae3b3a8b71337238e22ac1234aab4c2d8ae
Peter Smith <petergbsmith@gmail.com>
2020-02-26 18:34:20 -0600

    delete jwtConfig, passport.js, trashtagModel, userModel; fixed ghost text

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -39,7 +39,7 @@ const Register = props => {
   return (
     <div className='form-container'>
       <h1>
-        Account <span className='text-primary'>Register</span>
+        <span className='text-primary'>Register</span>
       </h1>
       <form onSubmit={onSubmit}>
         <div className='form-group'>
