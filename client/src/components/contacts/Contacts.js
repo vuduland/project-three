9170d91c7e9b7480c4d5c70ae2e655df098704f8
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-28 20:57:02 -0600

    copied over T files again

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -9,7 +9,7 @@ import ContactContext from '../../context/contact/contactContext';
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered, getContacts, loading } = contactContext;
+  const { contacts, getContacts, loading } = contactContext;
 
   useEffect(() => {
     getContacts();
@@ -24,31 +24,16 @@ const Contacts = () => {
 
   return (
     <Fragment>
-      {contacts !== null && !loading ? (
-        <TransitionGroup>
-          {filtered !== null
-            ? filtered.map(contact => (
-                <CSSTransition
-                  key={contact._id}
-                  timeout={500}
-                  classNames='item'
-                >
-                  <ContactItem contact={contact} />
-                </CSSTransition>
-              ))
-            : contacts.map(contact => (
-                <CSSTransition
-                  key={contact._id}
-                  timeout={500}
-                  classNames='item'
-                >
-                  <ContactItem contact={contact} />
-                </CSSTransition>
-              ))}
-        </TransitionGroup>
-      ) : (
-        <Spinner />
-      )}
+      <TransitionGroup>
+        <CSSTransition key={contact._id} timeout={500} classNames='item'>
+          <ContactItem contact={contact} />
+        </CSSTransition>
+        : contacts.map(contact => (
+        <CSSTransition key={contact._id} timeout={500} classNames='item'>
+          <ContactItem contact={contact} />
+        </CSSTransition>
+        ))
+      </TransitionGroup>
     </Fragment>
   );
 };
