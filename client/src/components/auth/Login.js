0d6d38ca5912b7cb7672d9f320d4c4f84914bf08
Victor Uland <53414263+vuduland@users.noreply.github.com>
2020-02-29 01:07:54 -0600

    remove 'Account' string

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -45,7 +45,7 @@ const Login = props => {
   return (
     <div className='form-container'>
       <h1>
-        Account <span className='text-primary'>Login</span>
+        <span className='text-primary'>Login</span>
       </h1>
       <form onSubmit={onSubmit}>
         <div className='form-group'>
